ae8373cd7d8f945a8f3bb0cce7dea1244b9c4652
Vinil Kappa ChandraBabu <156454928+vinil2405@users.noreply.github.com>
2024-04-11 21:34:22 -0500

    added code in app.js file

--- app.js
+++ app.js
@@ -4,6 +4,28 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  Account.findOne({ username: username })
+  .then(function (user){
+  if (err) { return done(err); }
+  if (!user) {
+  return done(null, false, { message: 'Incorrect username.' });
+  }
+  if (!user.validPassword(password)) {
+  return done(null, false, { message: 'Incorrect password.' });
+  }
+  return done(null, user);
+  })
+  .catch(function(err){
+  return done(err)
+  })
+  })
+  )
+
 var mongoose = require('mongoose');
 require('dotenv').config();
 const connectionString = process.env.MONGO_CON;
@@ -33,6 +55,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+  
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -42,6 +72,13 @@ app.use('/knife', knifeRouter);
 app.use('/grid',gridrsRouter);
 app.use('/knife',knifeModel)
 app.use('/resource',resourceRouter);
+// passport config
+// Use the existing connection
+// The Account model
+var Account =require('./model/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
