24a872ffd81e0aa2020f579f39666784a93bed7b
Vinil Kappa ChandraBabu <156454928+vinil2405@users.noreply.github.com>
2024-04-12 17:32:40 -0500

    added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,58 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+var Account = require('../model/account');
+router.get('/', function (req, res) {
+res.render('index', { title: 'knife App', user : req.user });
 });
-
-module.exports = router;
+router.get('/register', function(req, res) {
+res.render('register', { title: 'knife App Registration'});
+});
+router.post('/register', function(req, res) {
+Account.findOne({ username : req.body.username })
+.then(function (user){
+if(user != null ){
+console.log("exists " + req.body.username)
+return res.render('register', { title: 'Registration',
+message: 'Existing User', account : req.body.username })
+}
+let newAccount = new Account({ username : req.body.username });
+Account.register(newAccount, req.body.password, function(err, user){
+if (err) {
+console.log("db creation issue "+ err)
+return res.render('register', { title: 'Registration',
+message: 'access error', account : req.body.username })
+}
+if(!user){
+return res.render('register',{ title: 'Registration',
+message: 'access error', account : req.body.username })
+}
+})
+console.log('Sucess, redirect');
+res.redirect('/');
+})
+.catch(function (err){
+return res.render('register', { title: 'Registration',
+message: 'Registration error', account : req.body.username })
+})
+})
+router.get('/login', function(req, res) {
+  res.render('login', { title: 'Costume App Login', user : req.user });
+  });
+  router.post('/login', passport.authenticate('local'), function(req, res) {
+  res.redirect('/');
+  });
+  router.get('/logout', function(req, res) {
+  req.logout(function(err) {
+  if (err) { return next(err); }
+  res.redirect('/');
+  });
+  });
+  router.get('/ping', function(req, res){
+  res.status(200).send("pong!");
+  });
+  module.exports = router;
+  router.get('/ping', function(req, res){
+  res.status(200).send("pong!");
+  });
+  module.exports = router;
